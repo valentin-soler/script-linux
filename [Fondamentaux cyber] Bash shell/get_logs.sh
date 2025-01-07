b84dbcfed559b0f4784426cba5421d4ca2c2a135
Valentin <valentin.soler@laplateforme.io>
2025-01-07 13:53:10 +0100

    Ajout de commentaire

--- [Fondamentaux cyber] Bash shell/get_logs.sh
+++ [Fondamentaux cyber] Bash shell/get_logs.sh
@@ -6,8 +6,10 @@ filename="number_connection-${date_file:8:2}-${date_file:5:2}-${date_file:0:4}-$
 user_string="$(awk -F: '$3 >= 1000 && $3 < 60000 { print $1 }' /etc/passwd)"
 #Transformation en une liste, pour pouvoir check pour chaque user
 user_array=($user_string)
+# Pour chaque utilisateur, on met son nombre de connections
 for item in "${user_array[@]}"; do
     count=$(last | grep -c "$item")
     echo "Nombre de connexion pour $item : $count" >> "/tmp/$filename"
 done
+# On compresse et on met dans le dossier Backup
 cd /tmp/ && tar -cf "$HOME/Backup/$filename.tar" "$filename"
