1703c3f8f618e1b2f0ec9b126acd4cabd620b678
Valentin <valentin.soler@laplateforme.io>
2025-01-08 16:28:36 +0100

    aa

--- [Fondamentaux cyber] Bash shell/accessrights.sh
+++ [Fondamentaux cyber] Bash shell/accessrights.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 file="Shell_Userlist.csv" #Nom du fichier contenant les utilisateurs
-
+#Il doit ignorer la 1ére ligne !!
+ignore_line="True"
 while IFS= read -r line; do
-    #On récupére les données pour chaque utilisateur
-    prenom=$(echo $line | cut -d ',' -f 2)
-    nom=$(echo $line | cut -d ',' -f 3)
-    mdp=$(echo $line | cut -d ',' -f 4)
-    mdp_hashed=$(openssl passwd -1 $mdp) #Useradd veut un mdp hashé
-    type_user=$(echo $line | cut -d ',' -f 5)
-    username=$((prenom+nom)) #On crée le nom d'utilisateur avec le prénom et nom de l'utilisateur (on colle le nom et prénom)
-    #On crée l'utilisateur avec les infos récupérées
-    useradd -m -p "$mdp_hashed" $username
-    #-m -> Repertoire home -p -> POur le mdp 
-    #On met les droits superuser au Adminitrateur
-    if [ "$type_user" = "Admin" ]; then
-        usermod -aG sudo $username
+    if [ $ignore_line == "True" ]; then
+        ignore_line="False"
+    else
+        #On récupére les données pour chaque utilisateur
+        prenom=$(echo $line | cut -d ',' -f 2)
+        nom=$(echo $line | cut -d ',' -f 3)
+        mdp=$(echo $line | cut -d ',' -f 4)
+        mdp_hashed=$(openssl passwd -1 $mdp) #Useradd veut un mdp hashé
+        type_user=$(echo $line | cut -d ',' -f 5)
+        username=$((prenom+nom)) #On crée le nom d'utilisateur avec le prénom et nom de l'utilisateur (on colle le nom et prénom)
+        #On crée l'utilisateur avec les infos récupérées
+        useradd -m -p "$mdp_hashed" $username
+        #-m -> Repertoire home -p -> POur le mdp 
+        #On met les droits superuser au Adminitrateur
+        if [ "$type_user" = "Admin" ]; then
+            usermod -aG sudo $username
+        fi
     fi
 done < "$file"
